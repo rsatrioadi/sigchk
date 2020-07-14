72d0f6a8c5a87777cda887d1653154f3e55e0362
Satrio Adi Rukmono <satrio@informatika.org>
2020-07-14 16:31:40 +0700

    Optimized output formatting

--- sigchk.rb
+++ sigchk.rb
@@ -6,7 +6,6 @@ ARGV.each do |fname|
   pdf = Origami::PDF.read( fname, verbosity: Origami::Parser::VERBOSE_QUIET )
   pdf.each_field do |f| 
     print( f.T, " " )
-    f.Rect.each { |r| print( r, " " ) }
-    puts
+    puts( f.Rect.join( " " )) 
   end
 end
